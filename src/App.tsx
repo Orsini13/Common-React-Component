d663783b5f2e2fcd4416fead125df23571981cee
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-02-18 00:39:44 +0100

    1st edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import ListGroup from "./components/ListGroup";
+import { Alert } from "./components/Alert";
 
-function App(){
-  return <div><ListGroup/><ListGroup/></div>
+function App() {
+ 
+    return (
+    <div >
+      <Alert>
+      hello <span>world </span> 
+      </Alert>
+    </div>
+  );
 }
 export default App;
